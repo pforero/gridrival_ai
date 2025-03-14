ec532683fe62bc5282c4f017f95841fc0343e269
Pablo Forero <github46@pabloforero.eu>
2025-03-14 18:14:06 +0100

    refactor: remove unused modules

--- src/gridrival_ai/__init__.py
+++ src/gridrival_ai/__init__.py
@@ -15,13 +15,3 @@ Main Components
 __version__ = "0.1.0"
 __author__ = "Pablo Forero"
 __email__ = "github46@pabloforero.eu"
-
-from gridrival_ai.contracts import Contract
-from gridrival_ai.salary import SalaryManager
-from gridrival_ai.team import Team
-
-__all__ = [
-    "Team",
-    "Contract",
-    "SalaryManager",
-]
